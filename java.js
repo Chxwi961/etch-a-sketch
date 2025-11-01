3449a9f22dd70e9a13dedc67feffef153600fb01
Chxwi961 <char.elhayek@gmail.com>
2025-11-01 12:55:29 -0400

    Finished the basic layout of the site. Looks pretty nice and everything is organized and fitted properly

---
+++ java.js
@@ -0,0 +1,6 @@
+const sketchArea = document.querySelector("#sketchArea")
+const options = document.querySelector("#options")
+
+sketchArea.style.width = sketchArea.style.height = "600px"
+options.style.height = "600px"
+options.style.width = "140px"
