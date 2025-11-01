ba0426bc825e45be56f9c50a48d651a86261e3e2
Chxwi961 <char.elhayek@gmail.com>
2025-11-01 13:36:48 -0400

    Finished the grid layout logic + the slider to control the resolution + a reset button. This was done with innerHTML which isnt ideal but it works for now

--- java.js
+++ java.js
@@ -1,6 +1,50 @@
 const sketchArea = document.querySelector("#sketchArea")
 const options = document.querySelector("#options")
+const slider = document.querySelector("#slider")
+const sliderText = document.querySelector("#sliderText")
+const reset = document.querySelector("#reset")
 
-sketchArea.style.width = sketchArea.style.height = "600px"
-options.style.height = "600px"
-options.style.width = "140px"
+const GRIDSIDE = 600
+const optionWidth = 140
+
+sketchArea.style.width = sketchArea.style.height = options.style.height = `${GRIDSIDE}px`
+options.style.width = `${optionWidth}px`
+
+function changeColor() {
+    this.style.backgroundColor = 'black'
+}
+
+
+function createCells(perSide) {
+    const totalSquares = perSide ** 2 
+
+    for (let i=0; i<(totalSquares); i++) {
+        let newCell = document.createElement('div')
+        newCell.classList.add('cell')
+        newCell.style.width = newCell.style.height = `${GRIDSIDE/perSide}px`
+
+
+        newCell.addEventListener('mouseover' , changeColor)
+
+
+        sketchArea.appendChild(newCell)
+    }
+}
+
+createCells(16)
+
+function changeDisplay() {
+    sketchArea.innerHTML = ""
+    createCells(this.value)
+
+    sliderText.textContent = `${this.value} x ${this.value} (resolution)`
+}
+
+function resetArea() {
+    sketchArea.innerHTML = ""
+    createCells(slider.value)
+}
+
+slider.addEventListener('input', changeDisplay)
+reset.addEventListener('click', resetArea)
+
