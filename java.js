41081691b3317bbf091a11252492226b3c655254
Chxwi961 <char.elhayek@gmail.com>
2025-11-01 19:57:50 -0400

    Finished all! Added a rgb color slider, grid on/off, everything seems to be working great, fixed the click-dragging issue. Overall good project!

--- java.js
+++ java.js
@@ -3,16 +3,84 @@ const options = document.querySelector("#options")
 const slider = document.querySelector("#slider")
 const sliderText = document.querySelector("#sliderText")
 const reset = document.querySelector("#reset")
-
+const pencil = document.querySelector("#pencil")
+const eraser = document.querySelector("#eraser")
+const currenttool = document.querySelector("#tooltext")
+const colorSlider = document.querySelector("#colorSlider")
+const grid = document.querySelector("#grid")
 const GRIDSIDE = 600
 const optionWidth = 140
+
+
+let currentColor = 'black'
+let action = 'pencil'
+let isClicking = false;
+let currentGrid = 'ON'
+
 
 sketchArea.style.width = sketchArea.style.height = options.style.height = `${GRIDSIDE}px`
 options.style.width = `${optionWidth}px`
 
 function changeColor() {
-    this.style.backgroundColor = 'black'
+    let color = currentColor
+    if (isClicking == true && action == 'pencil') {
+        this.style.backgroundColor = `${color}` 
+    } else if (isClicking == true && action == 'eraser') {
+        this.style.backgroundColor = 'aliceblue'
+    }
 }
+
+function changeIsClicking(e) {
+    if (e.type == 'mousedown') {
+        isClicking = true
+    }else if (e.type == 'mouseup') {
+        isClicking = false 
+    } else if (e.type == 'mouseleave') {
+        isClicking = false
+    }
+}
+
+
+document.addEventListener('mousedown', changeIsClicking)
+document.addEventListener('mouseup', changeIsClicking)
+sketchArea.addEventListener('mouseleave', changeIsClicking)
+colorSlider.addEventListener('input', function () {
+    currentColor = this.value
+})
+
+pencil.addEventListener('click', () => {
+    action = 'pencil'
+    pencil.style.backgroundColor = 'rgb(70, 18, 18)'
+    eraser.style.backgroundColor = 'brown'
+    currenttool.textContent = 'Pencil'
+})
+eraser.addEventListener('click', () => {
+    action = 'eraser'
+    eraser.style.backgroundColor = 'rgb(70, 18, 18)'
+    pencil.style.backgroundColor = 'brown'
+    currenttool.textContent = 'Eraser'
+})
+
+grid.addEventListener('click', function() {
+    let cellArray = Array.from(sketchArea.children)
+    if (currentGrid == 'ON') {
+        currentGrid = 'OFF'
+        grid.style.backgroundColor = 'brown'
+        grid.textContent = `Grid (${currentGrid})`
+        cellArray.forEach(cell => {
+            cell.style.border = '0px';
+        })
+        
+        
+    } else {
+        currentGrid = 'ON'
+        grid.style.backgroundColor = 'rgb(69, 204, 69)'
+        grid.textContent = `Grid (${currentGrid})`
+        cellArray.forEach(cell => {
+            cell.style.border = '1px solid rgba(146, 141, 141, 0.377)'
+        })
+    }
+})
 
 
 function createCells(perSide) {
@@ -21,11 +89,9 @@ function createCells(perSide) {
     for (let i=0; i<(totalSquares); i++) {
         let newCell = document.createElement('div')
         newCell.classList.add('cell')
-        newCell.style.width = newCell.style.height = `${GRIDSIDE/perSide}px`
+        newCell.style.width = newCell.style.height = `${(GRIDSIDE/perSide)}px`
 
-
-        newCell.addEventListener('mouseover' , changeColor)
-
+        newCell.addEventListener('mouseleave', changeColor)
 
         sketchArea.appendChild(newCell)
     }
@@ -36,7 +102,12 @@ createCells(16)
 function changeDisplay() {
     sketchArea.innerHTML = ""
     createCells(this.value)
-
+    if (currentGrid == 'OFF') {
+        let newArray = Array.from(sketchArea.children)
+        newArray.forEach(cell => {
+            cell.style.border = '0px'
+        })
+    }
     sliderText.textContent = `${this.value} x ${this.value} (resolution)`
 }
 
